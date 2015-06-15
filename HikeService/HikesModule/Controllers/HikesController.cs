6eaf363183a98f2293d04f5a8943f1a1b08e9f3d
Vidya Sindhu Nagesh <vinagesh@microsoft.com>
2015-06-15 16:18:11 -0700

    Fixed validation error

--- HikeService/HikesModule/Controllers/HikesController.cs
+++ HikeService/HikesModule/Controllers/HikesController.cs
@@ -22,7 +22,7 @@ namespace HikeService.HikesModule.Controllers
 		public bool Post (string type, string user, [FromBody] UserData data)
 		{
 		    IDataStorageService dataStorageService = ServiceFactory.GetStorageService();
-		    if (!string.IsNullOrEmpty(data.Value) && data.Value.StartsWith("http://www.wta.org/go-hiking/hikes/"))
+		    if ((!string.IsNullOrEmpty(data.Value)) && data.Value.StartsWith("http://www.wta.org/") && data.Value.Contains("/go-hiking/hikes/"))
 		    {
 		        return dataStorageService.WriteUrl(user, type, data.Value);
 		    } else {
